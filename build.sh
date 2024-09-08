288d28364cffa050f48feb6a0fef5cf2c657e44f
Maël Kerbiriou <m431.kerbiriou@gmail.com>
2024-09-08 16:32:06 +0200

    use --artifact-dir for placing the output

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ fi
 
 if rustup toolchain list | grep -q "nightly-$TARGET"; then
     CARGO_FLAGS="+nightly $ACTION -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort"
+    if [ $ACTION = "build" ]; then
+        CARGO_FLAGS="$CARGO_FLAGS --artifact-dir=. -Z unstable-options"
+    fi
 else
     CARGO_FLAGS="$ACTION"
 fi
 
 cargo $CARGO_FLAGS --target "$TARGET" --release $@
-
-echo "$TARGET" > target/triple
--- install.sh
+++ install.sh
@@ -8,16 +8,12 @@ export PREFIX=${PREFIX:-/usr/local}
 # Path for the config file read by the systemd unit
 export CONFD=${CONFD:-/etc/conf.d}
 
-# build.sh puts the target triple in "target/triple". It is read for finding the
-# generated binary. When cargo build is used directly, this is empty and will
-# pull the binary from target/release/. However if you give an explicit --target
-# to cargo, you'll have to manually set this in the environment.
-TARGET=${TARGET:-"$(cat target/triple 2>/dev/null || true)"}
+ARTIFACT_DIR=${ARTIFACT_DIR:-.}
 
 _pkgname="rust-idle"
 PKGNAME=${PKGNAME:-"$_pkgname"}
 
-install -Dm755 "target/$TARGET/release/rust-idle" "$DESTDIR/$PREFIX/bin/rust-idle"
+install -Dm755 "$ARTIFACT_DIR/rust-idle" "$DESTDIR/$PREFIX/bin/rust-idle"
 strip -s "$DESTDIR/$PREFIX/bin/rust-idle"
 
 install -Dm644 README.md "$DESTDIR/$PREFIX/share/doc/$PKGNAME/README.md"
